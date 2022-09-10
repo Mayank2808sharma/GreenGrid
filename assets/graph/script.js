ef87090cec767611b5ea74dadd7c448e00401970
Mayank2808sharma <E21CSEU0886@bennett.edu.in>
2022-09-10 16:26:03 +0530

    UiWise Complete

--- assets/graph/script.js
+++ assets/graph/script.js
@@ -6,18 +6,18 @@ google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
   var data = google.visualization.arrayToDataTable([
     ["Months", "Average demand", "Electrical Availability"],
-    ["January", 31060, 5283],
-    ["February", 31133, 5281],
-    ["March", 29018, 5055],
-    ["April", 27133, 4499],
-    ["May", 26924, 3876],
-    ["June", 28374, 4278],
-    ["July", 30093, 4746],
-    ["August", 28575, 4402],
-    ["September", 28329, 4380],
-    ["October", 27213, 4768],
-    ["November", 28960, 5082],
-    ["December", 29530, 5017],
+    ["00:00 : 02:00", 29460, 7882],
+    ["02:00 : 04:00", 24629, 11715],
+    ["04:00 : 06:00", 23474, 12869],
+    ["06:00 : 08:00", 25592, 10751],
+    ["08:00 : 10:00", 31274, 5069],
+    ["10:00 : 12:00", 34271, 2072],
+    ["12:00 : 14:00", 34363, 1980],
+    ["14:00 : 16:00", 33033, 3310],
+    ["16:00 : 18:00", 32140, 4208],
+    ["18:00 : 20:00", 34559, 1784],
+    ["20:00 : 22:00", 36244, 109],
+    ["22:00 : 24:00", 33627, 2715],
   ]);
 
   var options = {
@@ -72,19 +72,11 @@ google.charts.load("current", { packages: ["corechart"] });
 google.charts.setOnLoadCallback(drawChart2);
 function drawChart2() {
   var data = google.visualization.arrayToDataTable([
-    ["Average demand", "Value Per month"],
-    ["January", 31060],
-    ["February", 31133],
-    ["March", 29018],
-    ["April", 27133],
-    ["May", 26924],
-    ["June", 28374],
-    ["July", 30093],
-    ["August", 28575],
-    ["September", 28329],
-    ["October", 27213],
-    ["November", 28960],
-    ["December", 29530],
+    ["Average demand", "Value Per Day"],
+    ["Morning", 30963],
+    ["Noon", 33442],
+    ["Evening", 33347],
+    ["Night", 28648],
   ]);
 
   var options2 = {
@@ -105,18 +97,10 @@ google.charts.setOnLoadCallback(drawChart3);
 function drawChart3() {
   var data = google.visualization.arrayToDataTable([
     ["Average demand", "Value Per month"],
-    ["January", 5283],
-    ["February", 5281],
-    ["March", 5055],
-    ["April", 4499],
-    ["May", 3876],
-    ["June", 4278],
-    ["July", 4746],
-    ["August", 4402],
-    ["September", 4380],
-    ["October", 4768],
-    ["November", 5082],
-    ["December", 5017],
+    ["Morning", 6319],
+    ["Noon", 3222],
+    ["Evening", 1603],
+    ["Night", 7116],
   ]);
 
   var options3 = {
